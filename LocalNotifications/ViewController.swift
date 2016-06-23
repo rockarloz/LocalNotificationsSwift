95d7cc734b036a7efec941a8d2a4b95444af40a2
Carlos <rockarlos@me.com>
2016-06-22 20:17:17 -0500

    star testing

--- LocalNotifications/ViewController.swift
+++ LocalNotifications/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+@IBAction func sendNotification(sender: UIButton) {
+        let localNotification = UILocalNotification()
+        localNotification.fireDate = NSDate(timeIntervalSinceNow: 5)
+        localNotification.alertBody = "new Blog Posted at iOScreator.com"
+        localNotification.timeZone = NSTimeZone.defaultTimeZone()
+        localNotification.applicationIconBadgeNumber = UIApplication.sharedApplication().applicationIconBadgeNumber + 1
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+    }
+
 
 }
 
