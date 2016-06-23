2829b13844fe14932e0314046297339b81b09eac
Carlos <rockarlos@me.com>
2016-06-22 19:17:10 -0500

    Initial Commit

---
+++ LocalNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Created by Carlos Castellanos on 22/06/16.
+//  Copyright © 2016 Carlos Castellanos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
